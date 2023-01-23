4a6ae6e9e8ba4a1f10a1d0ee9888c093c9eca5ea
MUHAMMEDANSIF <muhammadansif9633@gmail.com>
2023-01-23 08:14:32 +0530

    first commit

---
+++ model/chatster.modal.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+
+const chatsterSchema = mongoose.Schema({
+  user: String,
+  chatsters: [
+    {
+      personid: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+      },
+    },
+  ],
+});
+
+export default mongoose.model('chatster', chatsterSchema);
